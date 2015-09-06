a5232630dde8e56e49dc1f4914c15c4dd9e728cd
Matthew Molnar <matthew.molnar@gmail.com>
2015-09-06 02:19:18 +0000

    Trying to get onChange to fire for input

--- assets/app/components/taskItem.js
+++ assets/app/components/taskItem.js
@@ -3,15 +3,20 @@ var $ = require('../../vendor/jquery/dist/jquery.js');
 
 module.exports = React.createClass({
   handleSubmit: function(e) {
-    var text = React.findDOMNode(this.innertext).value.trim();
+    console.log('onChange caught');
+    var text = React.findDOMNode(this.refs.contents).value.trim();
     console.log('Save change: ' + text);
     //$.post('/TaskItems', (data) => this.setState({data:data}));
   },
   render: function() {
     var taskNodes = this.props.data.map(function (task) {
       return (
-        <div className="taskItemBox" contenteditable oninput={this.handleSubmit}>
-          {task.contents}
+        <div key={task.id} className="taskItemBox">
+          <input
+            type="text"
+            ref="contents"
+            defaultValue={task.contents}
+            onChange={this.handleSubmit} />
         </div>
       );
     });
