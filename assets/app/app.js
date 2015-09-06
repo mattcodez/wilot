0bc83bf0dbcba16d972dc9a6287b818da0b5acdb
Matthew Molnar <matthew.molnar@gmail.com>
2015-09-06 01:58:09 +0000

    Basic data-loading: static

--- assets/app/app.js
+++ assets/app/app.js
@@ -4,7 +4,19 @@ var _  = require('lodash');
 var React = require('react');
 var TaskItemBox = require('./components/taskItem.js');
 
+var data=[{contents:"yoo", id:"blah"}];
+
+var ListUI = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <TaskItemBox data={this.props.data} />
+      </div>
+    );
+  }
+});
+
 React.render(
-  <TaskItemBox />,
+  <ListUI data={data} />,
   document.getElementById('itemList')
 );
--- assets/app/components/taskItem.js
+++ assets/app/components/taskItem.js
@@ -2,10 +2,22 @@ var React = require('react');
 var $ = require('../../vendor/jquery/dist/jquery.js');
 
 module.exports = React.createClass({
+  handleSubmit: function(e) {
+    var text = React.findDOMNode(this.innertext).value.trim();
+    console.log('Save change: ' + text);
+    //$.post('/TaskItems', (data) => this.setState({data:data}));
+  },
   render: function() {
+    var taskNodes = this.props.data.map(function (task) {
+      return (
+        <div className="taskItemBox" contenteditable oninput={this.handleSubmit}>
+          {task.contents}
+        </div>
+      );
+    });
     return (
-      <div className="taskItemBox">
-        <h2>hi</h2>
+      <div className="nodeList">
+        {taskNodes}
       </div>
     );
   }
