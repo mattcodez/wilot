6552b93c7b8e9823381728cb043535675059f2ad
Matthew Molnar <matthew.molnar@gmail.com>
2015-09-07 16:08:01 +0000

    Getting data from server now

--- assets/app/app.js
+++ assets/app/app.js
@@ -1,22 +1,9 @@
 //var CONST_ES6_BUILD_PATH = './build/';
 
-var _  = require('lodash');
 var React = require('react');
-var TaskItemBox = require('./components/taskItem.js');
-
-var data=[{contents:"yoo", id:"blah"}];
-
-var ListUI = React.createClass({
-  render: function() {
-    return (
-      <div>
-        <TaskItemBox data={this.props.data} />
-      </div>
-    );
-  }
-});
+var ListUI = require('./components/ListUI.js');
 
 React.render(
-  <ListUI data={data} />,
+  <ListUI url={'/TaskItems'} />,
   document.getElementById('itemList')
 );
