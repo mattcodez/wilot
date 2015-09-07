80bb26e35b1a123ff47567fd75477a70f4bf2b09
Matthew Molnar <matthew.molnar@gmail.com>
2015-09-07 21:28:20 +0000

    Moving loop to listUI rather than taskItem so that we can .get('id') on taskItem for updates

--- assets/app/components/taskItem.js
+++ assets/app/components/taskItem.js
@@ -4,24 +4,21 @@ var $ = require('../../vendor/jquery/dist/jquery.js');
 module.exports = React.createClass({
   handleSubmit: function(e) {
     var text = React.findDOMNode(this.refs.contents).value.trim();
-    //$.post('/TaskItems', (data) => this.setState({data:data}));
+    $.post(
+      '/TaskItems/' + this.getAttribute('key'),
+      {contents:text},
+      (data) => this.setState({data:data})
+    );
   },
   render: function() {
-    var that = this;
-    var taskNodes = this.props.data.map(function (task) {
-      return (
-        <div key={task.id} className="taskItemBox">
-          <input
-            type="text"
-            ref="contents"
-            defaultValue={task.contents}
-            onChange={that.handleSubmit} />
-        </div>
-      );
-    });
+    let {task} = this.props;
     return (
-      <div className="nodeList">
-        {taskNodes}
+      <div key={task.id} className="taskItemBox">
+      <input
+      type="text"
+      ref="contents"
+      defaultValue={task.contents}
+      onChange={this.handleSubmit} />
       </div>
     );
   }
