e6e975d722b12d134f72fc819db61b8f635670ac
Matthew Molnar <matthew.molnar@gmail.com>
2015-09-18 01:38:40 +0000

    Work on adding a new task item

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+$(function(){
+  var taskList = new TaskList({el:'#taskList'});
+  $('form.new').on('submit', function(e){
+    e.preventDefault();
+
+    var form = $(this);
+    $.post('/TaskItems', form.serialize())
+    .then(function(taskItem){
+      taskList.addTask(taskItem);
+    })
+    .error(function(err){
+      alert(err);
+    });
+
+    return false;
+  });
+});
+
+var TaskList = function(opts){
+  this.el = $(opts.el);
+};
+TaskList.prototype = {};
+
+TaskList.prototype.addTask = function(taskItem){
+  this.el.append($('<div></div>').text(taskItem.contents));
+};
