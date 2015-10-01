9998026c4a73442054744e1cd8fa97cedeee97f2
mattcodez <xhr@io2g.com>
2015-10-01 01:02:27 +0000

    More glitch fixes

--- TODO.md
+++ TODO.md
@@ -1,5 +1,9 @@
-- [ ] Need to figure out where forms sit, always child of task?
+- [x] Need to figure out where forms sit, always child of task?
   TaskChildren? Both? Decide, then code.
-  - [ ] Markup planned but not quite implemented ye
+  - [x] Markup planned but not quite implemented ye
     - [x] Item only gets an input for sub items if it doesn't already
           if it doesn't already have sub items
+- [ ] Sub tasks aren't creating correctly
+  - [ ] Why doesn't each sub task have its own new box?
+    - [ ] Because they lack a "children" array, 
+       need to account for existence of empty array and no array
--- assets/js/TaskList.js
+++ assets/js/TaskList.js
@@ -25,8 +25,9 @@ TaskList.prototype.addTask = function(taskItem, container){
   var newTask = $('<div class="item"></div>')
     .text(taskItem.contents);
   
+  //Only add a form if task has no children
   //If taskItem has children, it's taskList will have the form
-  if (taskItem.children && taskItem.children.length > 0){
+  if (taskItem.children && taskItem.children.length === 0){
     newTask.append(
       this.getNewForm(taskItem.id)
     );
@@ -34,7 +35,7 @@ TaskList.prototype.addTask = function(taskItem, container){
   
   container.append(newTask);
   
-  if (taskItem.children){
+  if (taskItem.children && taskItem.children.length > 0){
     this.addTasks(taskItem.children, newTask, taskItem.id);
   }
 };
